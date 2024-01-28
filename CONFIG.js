e872dd94d1b3ad960b740b429b80cf9f1cc67c4d
Nitinb19 <157929035+Nitinb19@users.noreply.github.com>
2024-01-27 19:10:33 -0500

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Hello, I have something to say. </br> Will you be my Valentine!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+    "Please answer honestly. If you stay silent and leave, it means you AGREE.",
+  buttonYes: "I Agree",
+  buttonNo: "I Disagree",
+  titleModar: "Thank you.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Congrats asshat you chose right! Now get your ass over here so we can go out. </br> Ignore me all you'd like but at the end of the day, You are my whole world, always have and always will </br> I Love You <3",
 };
